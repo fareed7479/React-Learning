97c2e70068a32644feb4865c4607891d987c29fe
fareed7479 <fareedsk2233@gmail.com>
2026-08-21 10:03:55 +0530

    Rendered the Components using COnditions

--- ConditionalRenderingDemo/src/App.jsx
+++ ConditionalRenderingDemo/src/App.jsx
@@ -1,16 +1,13 @@
 import {useState} from 'react'
 
-import login from './components/LoginComponent/login'
+import Login from './components/LoginComponent/login'
+import Logout from './components/LoginComponent/logout'
 import './App.css'
 
 const App = () => {
   const [isLoggedIn, setIsLoggedIn] = useState(true)
-
-
-
   //Conditional Rendering of the Button based on the State Value.
   // const renderButton = () =>{
-
   //   //we are using a function to display the based on if statement.
   //   if (isLoggedIn) {
   //     return <button onClick = {()=> setIsLoggedIn(false)}>Logout</button>
@@ -19,14 +16,21 @@ const App = () => {
   // }
   return (
     <div className="container">
-      <h1>Hello, User</h1>
+      {isLoggedIn ? (
+        <Login name="John" greeting="Welcome" />
+      ) : (
+        <Logout />
+      )}
       {/* //Functional Component Rendering based on the State Value. */}
       {/* {renderButton()} */}
       {/* Using terinary operator to display the button based on the state value. */}
       {isLoggedIn ? (
-        <button onClick={() => setIsLoggedIn(false)}>Logout</button>
+        <button onClick={() => setIsLoggedIn(false)}>LogOut</button>
       ) : (
-        <button onClick={() => setIsLoggedIn(true)}>Login</button>
+        // <button onClick={() => setIsLoggedIn(true)}>Login</button>
+        //Null is Used when i want the user to logged in and couldn't logges out.
+        //So, the button will not be displayed when the user is logged in.
+        null
       )}
     </div>
   )
